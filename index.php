7fb8fda6b481ffe482b03a4116e6f801ddb2478b
Sabri Bin Saep <sabrisaep@gmail.com>
2021-10-11 12:56:14 +0800

    bmi belum siap

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Function</title>
+    <style>
+        * {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Function</h1>
+
+    <?php
+
+    function hello()
+    {
+        echo 'Hello';
+    }
+
+    function hai($nama)
+    {
+        return 'Hello ' . $nama;
+    }
+
+    hello();
+    echo '<br>';
+    echo hai('Ali');
+
+    ?>
+    <p><a href="contoh.php">CONTOH</a></p>
+
+</body>
+
+</html>
